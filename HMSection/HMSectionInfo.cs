5c76486227381625a7016474afac85e1d4615448
hMatejak <honza.m46@gmail.com>
2024-02-10 18:55:23 +0100

    Working solution - only one contour.

--- HMSection/HMSectionInfo.cs
+++ HMSection/HMSectionInfo.cs
@@ -13,14 +13,18 @@ namespace HMSection
         public override Bitmap Icon => Properties.Resources.main;
 
         //Return a short string describing the purpose of this GHA library.
-        public override string Description => "";
+        public override string Description => "HMSection is a grasshopper toolbox" +
+            "for the analysis of cross-sections. Uses CrossSection.net prackage," +
+            " that is based on python package sectionproperties.";
 
         public override Guid Id => new Guid("a6cb1a60-393c-4e9b-967e-67c31b98d7e4");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "Jan Matěják";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "honza.m46@gmail.com";
+
+        public override string AssemblyVersion => "0.5.0";
     }
 }
