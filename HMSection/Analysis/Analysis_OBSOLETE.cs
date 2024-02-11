ca129e5cdcbf9b1c6c56ba4e5e6849242e7f0bbf
hMatejak <honza.m46@gmail.com>
2024-02-11 10:45:11 +0100

    Licence added

--- HMSection/Analysis/Analysis_OBSOLETE.cs
+++ HMSection/Analysis/Analysis_OBSOLETE.cs
@@ -1,3 +1,27 @@
+/// <copyright>
+/// https://github.com/hMatejak/HMSection
+/// MIT License
+///
+/// Copyright(c) 2024 Jan Matìják
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+/// </copyright>
+
 using Grasshopper;
 using Grasshopper.Kernel;
 using System;
@@ -34,6 +58,8 @@ namespace HMSection.Analysis
         protected override System.Drawing.Bitmap Icon => Properties.Resources.analysis;   
 
         public override Guid ComponentGuid => new Guid("6456fb97-626b-40c1-b02b-82f6f732d07c");
+
+        public override GH_Exposure Exposure => GH_Exposure.hidden;
 
 
         /// <summary>
--- HMSection/Analysis/Settings.cs
+++ HMSection/Analysis/Settings.cs
@@ -1,3 +1,27 @@
+/// <copyright>
+/// https://github.com/hMatejak/HMSection
+/// MIT License
+///
+/// Copyright(c) 2024 Jan Matìják
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+/// </copyright>
+
 using Grasshopper;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
@@ -41,9 +65,8 @@ namespace HMSection.Analysis
             pManager.AddBooleanParameter("Conforming Delaunay", "CD", "Conforming Delaunay", GH_ParamAccess.item, true);
             pManager.AddNumberParameter("Plastic axis accuracy", "PA", "Plastic axis accuracy", GH_ParamAccess.item, 1E-5);
             pManager.AddIntegerParameter("Plastic axis max iterations", "PI", "Plastic axis max iterations", GH_ParamAccess.item, 500);
-            pManager.AddBooleanParameter("Plastic analysis", " PA?", "Determines if plastic analysis s run", GH_ParamAccess.item, true);
-            pManager.AddBooleanParameter("Warping analysis", " WA?", "Determines if warping analysis s run", GH_ParamAccess.item, true);
-
+            pManager.AddBooleanParameter("Plastic analysis", " PA?", "Determines if plastic analysis s run", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Warping analysis", " WA?", "Determines if warping analysis s run", GH_ParamAccess.item, false);
         }
 
         /// <summary>
